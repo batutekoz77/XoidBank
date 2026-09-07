b1cf4f8829499b5aa87b12e93a55ab708849ddfb
Batu Teköz <137097693+batutekoz77@users.noreply.github.com>
2026-09-08 00:37:18 +0200

    Delete XoidBank directory

--- XoidBank/Application/Register/Register.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../../Include/Crow.hpp"
-#include "../../Database/Users.hpp"
-#include "../../Include/RateLimit/RateLimit.hpp"
-
-class RegisterRouter {
-public:
-    static void RegisterRoutes(CrowApp& app, UsersBase& usersBase, RateLimiter& rateLimiter);
-};
